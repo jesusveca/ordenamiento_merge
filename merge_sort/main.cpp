cf7751d27c1c80a533d8fec296cb13524cb48fac
jesusveca <jerikerl10@gmail.com>
2016-03-23 06:34:34 -0500

    Initial Commit

---
+++ merge_sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  merge_sort
+//
+//  Created by jesus erick vera callme on 23/03/16.
+//  Copyright © 2016 jesus erick vera callme. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
